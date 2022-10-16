141a7df0eb5015fdfa5c8c84eee801d5379df91c
Sergey <saxum2010@gmail.com>
2022-10-16 23:17:20 +0300

    'First'

---
+++ src/SimpleCrudGeneratorServiceProvider.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Saxum2010\SimpleCrudGenerator;
+
+use Illuminate\Support\ServiceProvider;
+
+class SimpleCrudGeneratorServiceProvider extends ServiceProvider
+{
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
+
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__ . '/../config/simplecrudgenerator.php' => config_path('simplecrudgenerator.php'),
+        ]);
+
+        $this->publishes([
+            __DIR__ . '/../publish/views/' => base_path('resources/views/'),
+        ]);
+
+        $this->publishes([
+            __DIR__ . '/stubs/' => base_path('resources/simple-crud-generator/'),
+        ]);
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->commands(
+            'Saxum2010\SimpleCrudGenerator\Commands\SimpleCrudCommand'
+        );
+    }
+}
